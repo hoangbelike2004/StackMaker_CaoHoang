1fa55a43e24669fbfc63c4064d1877f006e2425b
hoangbelike2004 <hoangbelike04@gmail.com>
2024-04-13 12:50:45 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerConTroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+enum MoveDirection
+{
+    Right,
+    Left,
+    Backward,
+    Forward,
+}
+public class PlayerConTroller : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
